14dc00f260001ad13b8a878b50298977cb914568
taylorA <andrew.joshua.taylor@gmail.com>
2014-02-25 14:58:04 -0600

    Added 3rd level drop down accordions

--- js/TwentyFourteen_Accordion_Menu.js
+++ js/TwentyFourteen_Accordion_Menu.js
@@ -17,16 +17,12 @@ function TwentyFourteen_Accordion_Menu(){
 	var parent_li = jQuery(this).parent('li');
 	
 	// if sub menu does not exist in parent li
-	if ( !jQuery('ul.sub-menu', parent_li).length ) {
-	    // show sub menus
-	    jQuery('#menu-main-menu ul.sub-menu').show();    
+	if ( !jQuery('ul.sub-menu', parent_li).first().length ) {
 	    return;
 	}
 	
 	// if sub menu is already active, bail
 	if ( parent_li.hasClass('sub-menu-active') ){
-	    // show sub menus
-	    jQuery('#menu-main-menu ul.sub-menu').show();
 	    return;
 	}
 	
@@ -34,13 +30,51 @@ function TwentyFourteen_Accordion_Menu(){
 	e.preventDefault();
 	
 	// store current sub menu in variable
-	var current_submenu = jQuery('ul.sub-menu', parent_li);
+	var current_submenu = jQuery('ul.sub-menu', parent_li).first();
 	
 	// slide up non-current sub menus
-	jQuery('#menu-main-menu ul.sub-menu').not(current_submenu).slideUp(function(){
+	jQuery('#menu-main-menu > li > ul.sub-menu').not(current_submenu).slideUp(function(){
 	    
-	    // remove sub-menu-active class from all items except current parent li
-	    jQuery('#menu-main-menu li').not(parent_li).removeClass('sub-menu-active');
+	    // remove sub-menu-active class from all first level items except current parent li
+	    jQuery('#menu-main-menu > li').not(parent_li).removeClass('sub-menu-active');
+	    
+	});
+	
+	// slide down current sub menu
+	current_submenu.slideDown(function(){
+	    // add sub-menu-active to current parent li
+	    parent_li.addClass('sub-menu-active');
+	});
+	
+    });
+    
+    // second level nav click function
+    jQuery('#menu-main-menu ul.sub-menu > li > a').click(function(e){
+	
+	// store parent li to variable
+	var parent_li = jQuery(this).parent('li');
+	
+	// if sub menu does not exist in parent li
+	if ( !jQuery('ul.sub-menu', parent_li).first().length ) {
+	    return;
+	}
+	
+	// if sub menu is already active, bail
+	if ( parent_li.hasClass('sub-menu-active') ){
+	    return;
+	}
+	
+	// stop link click
+	e.preventDefault();
+	
+	// store current sub menu in variable
+	var current_submenu = jQuery('ul.sub-menu', parent_li).first();
+	
+	// slide up non-current sub menus
+	jQuery('#menu-main-menu ul.sub-menu > li > ul.sub-menu').not(current_submenu).slideUp(function(){
+	    
+	    // remove sub-menu-active class from all second level items except current parent li
+	    jQuery('#menu-main-menu ul.sub-menu > li').not(parent_li).removeClass('sub-menu-active');
 	    
 	});
 	
