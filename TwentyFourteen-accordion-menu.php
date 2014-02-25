198f3fa5db91c72acbdad87b6bd92698fd0e39a0
taylorA <andrew.joshua.taylor@gmail.com>
2014-02-25 12:01:09 -0600

    Initial plugin file

---
+++ TwentyFourteen-accordion-menu.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: TwentyFourteen Accodion Menu
+ * Description: Adds accordion functionality to the Top primary menu in the TwentyFourteen theme on mobile
+ * Version: 1.0
+ * Author: Advice Interactive Group
+ * Author URI: http://www.adviceinteractivegroup.com
+ * License: GPL2
+ * Copyright 2014  Advice Interactive Group  (email : info@adviceineractive.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as 
+ * published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
